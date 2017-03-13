ab9dd0e5ade4325896f1d4ae71bee72c7af644d6
sampath-tring <sampath.n@tringapps.com>
2017-03-13 16:31:37 +0530

    device details added :rocket:

--- obox_router_configure.py
+++ obox_router_configure.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import subprocess
 
 MAC_ADDRESS_LIST = []
 
@@ -14,13 +15,14 @@ parser.add_argument('-ip', '--ip', help="IP address", required=False)
 parser.add_argument('-mac', '--mac', help="MAC address", required=False)
 parser.add_argument('-url', '--url', help="URL", required=False)
 parser.add_argument('-b', '--b', help="block", required=False, action='store_true')
-parser.add_argument('-device_details', '--device_details', help="lisout existing device", required=False)
+parser.add_argument('-details', '--details', help="lisout existing device", required=False, action='store_true')
 arguments = parser.parse_args()
 
 ip_address = arguments.ip
 mac_address = arguments.mac
 url = arguments.url
 block =arguments.b
+device_details = arguments.details
 
 def ip_address_blocker_and_unblocker(ip_address, block=None):
     if block:
@@ -57,7 +59,11 @@ def url_blocker_and_unblocker(url, block=None):
         unblock_command = "" % (url) 
         command_executer = os.system(unblock_command) 
         save_command = "service iptables save"
-        command_executer = os.system(save_command)   
+        command_executer = os.system(save_command)  
+
+def device_details_listouter():
+    command = "arp"
+    command_executer = subprocess.call(command)         
 
 
 if __name__ == "__main__":
@@ -67,4 +73,7 @@ if __name__ == "__main__":
         mac_id_blocker_and_unblocker(mac_address, block)
     if url:
         url_blocker_and_unblocker(url, block)        
+    if device_details:
+        device_details_listouter()
+
 
