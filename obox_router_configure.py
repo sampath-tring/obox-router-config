a0accbf266a09d070f830abd7aff829e88b34036
sampath-tring <sampath.n@tringapps.com>
2017-03-17 14:07:42 +0530

    log file creator added :heavy_plus_sign:

--- obox_router_configure.py
+++ obox_router_configure.py
@@ -8,7 +8,10 @@ from commands import ip_block_command as ibc, ip_unblock_command as inc, \
 mac_block_command as mbc, mac_unblock_command as muc, url_block_command as ubc, \
 url_unblock_command as uuc, wifi_access_killer as wak, dhcp_enable_command as dec, \
 dhcp_disable_command as ddc
-from paths import WIFI_ACCESS_POINT as wap, DHCP_LEASE_FILE as dlf, DHCP_LEASE_FILE_PATH as dlfp
+from paths import WIFI_ACCESS_POINT as wap, DHCP_LEASE_FILE as dlf, \
+DHCP_LEASE_FILE_PATH as dlfp, IP_ADDRESS_LOG_FILE as ip_log_file, \
+MAC_ADDRESS_LOG_FILE as mac_log_file, \
+URL_LOG_FILE as url_log_file
 from termcolor import colored
 
 #getting ipaddress, macaddress, url
@@ -45,6 +48,8 @@ def ip_address_blocker_and_unblocker(ip_address, block=None):
         command_executer = os.system(block_command)
         save_command = "service iptables save"
         command_executer = os.system(save_command)
+        #adding the ip to black list log file
+        blacklist_log_file_creator(ip_address=ip_address)
         print colored("Device %s is blocked" % (ip_address), 'red')
     else:
         unblock_command = "iptables -D INPUT -s %s -j DROP" % (ip_address)
@@ -59,6 +64,8 @@ def mac_id_blocker_and_unblocker(mac_address, block=None):
         command_executer = os.system(block_command)
         save_command = "service iptables save"
         command_executer = os.system(save_command)
+        #adding the mac id to black list log file
+        blacklist_log_file_creator(mac_address=mac_address)
         print colored("Device %s is blocked" % (mac_address), 'red')
     else:
         unblock_command =  muc % (mac_address) 
@@ -74,6 +81,8 @@ def url_blocker_and_unblocker(url, block=None):
         command_executer = os.system(block_command)
         save_command = "service iptables save"
         command_executer = os.system(save_command)
+        #adding url to black list log file
+        blacklist_log_file_creator(url=url)
         print colored("url %s is blocked" % (url), 'red')
     else:
         unblock_command = "" % (url) 
@@ -81,6 +90,36 @@ def url_blocker_and_unblocker(url, block=None):
         save_command = "service iptables save"
         command_executer = os.system(save_command)  
         print colored("url %s is unblocked" % (url), 'blue')
+
+def blacklist_log_file_creator(ip_address=None, mac_address=None, url=None):
+    if ip_address:
+        file_opener = open(ip_log_file, "a")
+        write = file_opener.write(ip_address+"\n")
+        file_opener.close()
+    elif mac_address:
+        file_opener = open(mac_log_file, "a")
+        write = file_opener.write(mac_address+"\n")
+        file_opener.close()
+    elif url:
+        file_opener = open(url_log_file, "a")
+        write = file_opener.write(url+"\n")
+        file_opener.close()              
+
+def blacklist_vanisher():
+    ip_file_opener = open(ip_log_file, "r")
+    for ip_address in ip_file_opener.readlines():
+        ip_address_blocker_and_unblocker(ip_address=ip_address)
+    ip_file_opener.close()
+
+    mac_file_opener = open(mac_log_file, "r")
+    for mac_address in mac_file_opener.readlines():
+        mac_id_blocker_and_unblocker(mac_address=mac_address)
+    mac_file_opener.close()    
+
+    url_file_opener = open(url_log_file, "r")
+    for url in url_file_opener.readlines():
+        url_blocker_and_unblocker(url=url) 
+    url_file_opener.close()       
 
 def device_details():
     if os.path.exists(dlf):
@@ -97,11 +136,12 @@ def device_details():
                 mac_address_list.append(line.replace("hardware ethernet ", "").replace(";", "").replace("\n", ""))
             if "binding state" in line and "next" not in line and "rewind" not in line:
                 binding_list.append(line.replace("binding state", "").replace(";", "").replace("\n", ""))
+        file_opener.close()
         # if "client-hostname" in line:
         #   hostname_list.append(line.replace("client-hostname ","").replace(";","").replace('"', ''))  
-            listed_value = [list(values) for values in zip(ip_address_list,mac_address_list, binding_list)]
-            listed_value_wo_duplicates = list(listed_value for listed_value,_ in itertools.groupby(listed_value))
-            final_details_list = []
+        listed_value = [list(values) for values in zip(ip_address_list,mac_address_list, binding_list)]
+        listed_value_wo_duplicates = list(listed_value for listed_value,_ in itertools.groupby(listed_value))
+        final_details_list = []
         for data in listed_value_wo_duplicates:
             final_details_list.append(data) if "   free" not in data[2] else ""
     
@@ -148,6 +188,7 @@ if __name__ == "__main__":
     if factory_reset:
         access_point_vanisher() 
         connected_device_vanisher()
+        blacklist_vanisher()
     if dhcp:
         dhcp_enabler_and_disabler(dhcp_enable)
     
--- paths.py
+++ paths.py
@@ -5,4 +5,8 @@ DHCP_LEASE_FILE = '/var/lib/dhcp/dhcpd.leases'
 
 DHCP_LEASE_FILE_PATH = '/var/lib/dhcp/'
 
+#log file
+IP_ADDRESS_LOG_FILE = 'ipaddress.log' 
+MAC_ADDRESS_LOG_FILE = 'macaddress.log'
+URL_LOG_FILE = 'url.log'
 
